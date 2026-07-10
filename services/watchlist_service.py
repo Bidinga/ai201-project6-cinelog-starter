eca89ba7d8137cacdd2fd463054188a95023b95d
bidinga <artxque@gmail.com>
2026-07-10 18:09:39 -0400

    refactor: sort watchlist by date added to match collection ordering

--- services/watchlist_service.py
+++ services/watchlist_service.py
@@ -49,7 +49,9 @@ def add_to_watchlist(user_id, film_id):
 
 def get_watchlist(user_id):
     """
-    Return all films on a user's watchlist.
+    Return all films on a user's watchlist, sorted by date added (newest first).
+
+    Ordering matches get_collection() so the two feeds behave consistently.
 
     Args:
         user_id (str): UUID of the user.
@@ -60,8 +62,7 @@ def get_watchlist(user_id):
     entries = (
         WatchlistEntry.query
         .filter_by(user_id=user_id)
-        .join(Film)
-        .order_by(Film.title.asc())
+        .order_by(WatchlistEntry.date_added.desc())
         .all()
     )
 
